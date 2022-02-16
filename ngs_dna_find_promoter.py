610093d43cfacfd642cb0ecfcab0cee350050627
GMueller91 <grantmueller@gmail.com>
2022-02-16 00:31:58 -0500

    Updated regex match pattern for promoter.

--- ngs_dna_find_promoter.py
+++ ngs_dna_find_promoter.py
@@ -1,8 +1,9 @@
-# Grant Mueller - 14Feb22 -----------------------------------------------------------------------------------------+-
+#!/usr/bin/env python3
+# Grant Mueller - 14Feb22 --------------------------------------------------------------------------+-
 # Use this script to generate a random chromosome of 250 million bases and search for a promoter
-#-+----------------------------------------------------------------------------------------------------------------+-
+# --------------------------------------------------------------------------------------------------+-
 
-#Use the Python random module to generate a randomized sequence of 250 million bases: [atcg] , then find promoter.
+#Use the Python random module to generate a randomized sequence of 250 million bases a,t,c,g.
 import random
 bases = ['a', 't', 'c', 'g']
 sequenceList = []
@@ -10,39 +11,36 @@ for n in range(0, 250000000):
     sequenceList.append(random.choice(bases))
 chromosome = "".join(sequenceList)
 
-#Search the randomly generated chromosome for a specified promoter sequence using regex and measure the ex time. 
+print("Generated artificial chromosome of length: " + str(len(chromosome)) +
+      " bases!\n\nFirst and last three codons:\n" + chromosome[:9] + " ... " + chromosome[-9:])
+
+print("\nSearching for promoters with the structure:\n" +
+      "   <-- upstream                                                         downstream -->   \n" +
+      "5'-****TTGACA*************************TATAAT****GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG****-3'\n" +
+      "        -35                            -10          Gene to be transcribed\n")
+
+# Use regex to search the randomly generated chromosome for a promoter sequence and measure the time. 
 import time, re
-promoter = 'ttgaca.{15, 25}tataat' 
+promoter = 'ttgaca.{15,25}tataat' 
 t1 = time.time() 
 result = re.finditer(promoter, chromosome) 
 t2 = time.time() 
-print("Search time was ", (t2-t1), " seconds")
+print("Search time was ", (t2-t1), " seconds\n")
 
-#--------------------------------------------------------------------------------------------------------------------
-#Expected output: Search time was 0.00100994110107 seconds
-
-#Use match objects. 
+# Use match objects to return the start, end, and sequence for each identified promoter.
 nmatches = 0 
 for match in result:
      nmatches += 1     
      print(match.start(), match.end(), match.group())
-print("Number of search hits = ", nmatches)
+print("\nNumber of search hits = ", nmatches)
 
-#--------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------+-
 '''Expected output shown below:
-1199566 1199603 ttgacactcacatcatcagagccccacatagtataat
-2103278 2103308 ttgacacacacagggtttgtgatttataat
-3702112 3702141 ttgacactctttcaaaccaggactataat
+134977 135014 ttgacagagagatagggttggtgcgggaccttataat
+1595924 1595959 ttgacattaacgttaaccctctgttgacctataat
+2125530 2125566 ttgacacccgcaaaaaagcgtgcttcgagctataat
 ...
-245627316 245627350 ttgacaaggtctccgtggccccggctattataat
-246256184 246256220 ttgacaggattcctctcgttaattacatcgtataat
-248653641 248653674 ttgacaaccgggctcgtaacgtattagtataat
-Number of search hits =  185'''
-
-# Output from 14Feb22
-# Search time was  0.0009996891021728516  seconds
-# Number of search hits =  0
-
-# -------------------------------------------------------------------------------------------------------------------
-# Link to article with source code:
-# https://www.digitalbiologist.com/blog/2018/07/python-for-genomics-and-next-generation-sequencing.html
+246078993 246079023 ttgacacctcaactttgaaacgagtataat
+247451725 247451760 ttgacatacagttctgtccacataaaaattataat
+247637482 247637515 ttgacagattcagggccaggaaccagctataat
+Number of search hits =  176'''
